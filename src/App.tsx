fb3fbfb35b893fee4b5ceb56b5bc83ed753cc5b7
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2023-07-08 12:00:30 -0300

    update resume + set app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Header from './components/Header'
-import React from  'react'
+import React, { useEffect } from  'react'
 import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
@@ -7,10 +7,13 @@ import Contact from './components/Contact'
 
 function App() {
 
-  return (
+  useEffect(() => {
+    document.title = "ruza's portfolio"
+  })
 
+  return (
+    
     <div className="bg-base-100 text-primary-content flex flex-col items-center min-h-screen">
-      <title>ruza's portfolio</title>
       <Header />
       <main className='p-4 flex flex-col gap-10 '>
       <Hero/>
